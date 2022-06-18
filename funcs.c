e90825465adff23f2a47d3c468615a5500f14f33
AlanGacihi <alanmamicha@gmail.com>
2022-06-18 03:28:33 -0700

    Add rotr

--- funcs.c
+++ funcs.c
@@ -27,3 +27,48 @@ void nop(__attribute__((unused)) stack_t **stack,
 	 __attribute__((unused)) unsigned int line_number)
 {
 }
+
+/**
+ * rotl - rotates the stack to the top
+ * @stack: Stack
+ * @line_number: Line number
+ */
+void rotl(stack_t **stack, __attribute__((unused)) unsigned int line_number)
+{
+	stack_t *tmp, *new;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return;
+
+	tmp = *stack;
+	new = (*stack)->next;
+	new->prev = NULL;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = *stack;
+	(*stack)->next = NULL;
+	(*stack)->prev = tmp;
+	(*stack) = new;
+}
+
+/**
+ * rotr - Rotate right, bottom element becomes first
+ * @stack: Stack
+ * @line_number: Line number
+ */
+void rotr(stack_t **stack, __attribute__((unused)) unsigned int line_number)
+{
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return;
+
+	tmp = *stack;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	tmp->next = *stack;
+	tmp->prev->next = NULL;
+	tmp->prev = NULL;
+	(*stack)->prev = tmp;
+	(*stack) = tmp;
+}
--- monty.h
+++ monty.h
@@ -57,6 +57,8 @@ void mod(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
 void run(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void push_q(stack_t **stack, unsigned int line_number);
